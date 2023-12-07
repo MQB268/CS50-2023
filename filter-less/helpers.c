3c22efa0173ed428340732154118a1b5bcea694a
MQBao89 <bao.mai@zodinet.com>
2023-12-07 23:19:39 +0700

    fiftyville, filter, inheritance, mario

---
+++ filter-less/helpers.c
@@ -0,0 +1,104 @@
+#include "helpers.h"
+#include "math.h"
+
+// Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            float blue = image[i][j].rgbtBlue;
+            float green = image[i][j].rgbtGreen;
+            float red = image[i][j].rgbtRed;
+            int average = round((red + blue + green) / 3);
+            image[i][j].rgbtBlue = image[i][j].rgbtGreen = image[i][j].rgbtRed = average;
+        }
+    }
+    return;
+}
+
+// Convert image to sepia
+void sepia(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            RGBTRIPLE temp = image[i][j];
+            double sepiaRed = 0.393 * temp.rgbtRed + 0.769 * temp.rgbtGreen + 0.189 * temp.rgbtBlue;
+            double sepiaGreen = 0.349 * temp.rgbtRed + 0.686 * temp.rgbtGreen + 0.168 * temp.rgbtBlue;
+            double sepiaBlue = 0.272 * temp.rgbtRed + 0.534 * temp.rgbtGreen + 0.131 * temp.rgbtBlue;
+            image[i][j].rgbtRed = round(sepiaRed) > 255 ? 255 : round(sepiaRed);
+            image[i][j].rgbtGreen = round(sepiaGreen) > 255 ? 255 : round(sepiaGreen);
+            image[i][j].rgbtBlue = round(sepiaBlue) > 255 ? 255 : round(sepiaBlue);
+        }
+    }
+    return;
+}
+
+// Reflect image horizontally
+void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width / 2; j++)
+        {
+            RGBTRIPLE temp = image[i][j];
+            image[i][j] = image[i][width - (j + 1)];
+            image[i][width - (j + 1)] = temp;
+        }
+    }
+    return;
+}
+
+// Blur image
+void blur(int height, int width, RGBTRIPLE image[height][width])
+{
+    RGBTRIPLE temp[height][width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            temp[i][j] = image[i][j];
+        }
+    }
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            float sum_red;
+            float sum_blue;
+            float sum_green;
+            int counter;
+            sum_red = sum_blue = sum_green = counter = 0;
+
+            for (int k = -1; k < 2; k++)
+            {
+                for (int l = -1; l < 2; l++)
+                {
+                    if (i + k < 0 || i + k >= height)
+                    {
+                        continue;
+                    }
+
+                    if (j + l < 0 || j + l >= width)
+                    {
+                        continue;
+                    }
+
+                    sum_red += temp[i + k][j + l].rgbtRed;
+                    sum_blue += temp[i + k][j + l].rgbtBlue;
+                    sum_green += temp[i + k][j + l].rgbtGreen;
+                    counter++;
+                }
+            }
+
+            image[i][j].rgbtRed = round(sum_red / counter);
+            image[i][j].rgbtGreen = round(sum_green / counter);
+            image[i][j].rgbtBlue = round(sum_blue / counter);
+        }
+    }
+    return;
+}
---
+++ mario-more/mario.c
@@ -0,0 +1,36 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // Ask user input height
+    int height;
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (height < 1 || height > 8);
+
+    // Print pyramid of blocks
+    for (int i = 1; i <= height; i++)
+    {
+        for (int j = 0; j < height - i; j++)
+        {
+            printf(" ");
+        }
+
+        for (int k = 0; k < i; k++)
+        {
+            printf("#");
+        }
+
+        printf("  ");
+
+        for (int m = 0; m < i; m++)
+        {
+            printf("#");
+        }
+
+        printf("\n");
+    }
+}
